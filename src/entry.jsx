28e65ff541149c21d30ced50ff9fc488b80e386c
Sascha <baecker.sascha@googlemail.com>
2025-04-26 23:15:08 +0200

    End of Fundamentals Part

---
+++ src/entry.jsx
@@ -0,0 +1,13 @@
+import {createRoot} from 'react-dom/client';
+import {StrictMode} from "react";
+
+import './app.css';
+import { App } from './App.jsx';
+
+
+createRoot(document.getElementById('app')).render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+);
+
